03eb360cf172b2b6f118d817ebe760da3691939d
Expo <crdailey01@gmail.com>
2023-11-11 17:26:09 -0800

    review. still hard

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,17 @@ public class Main {
     }
 }
 
+
+// reviewing this, just getting whiteboard concept in my head for now since trying to understand the algorithm conceptually
+// and then also the code is too much. so, here's the premise of the algorithm.
+// preorder = [3,9,20,15,7]
+// inorder = [9,3,15,20,7]
+// in order will have its values mapped as keys with index as the value. will then go through pre-order[] and see
+// that pre-order will give all the parent nodes 3,9,20. start with value three and retrieve inOrder index of 1
+// where [9] will be in L branch and [25,30,7] will be in R branch. again, we see 9 from pre-order will be index 0
+// of inOrder and range will be modified to indicate and 9 has no valid nodes to L or right. then to 20 in preorder,
+// we see so is index 3 in inOrder and 15 and 7 are valid nodes with clamped range. finally, leaf nodes 15 and 7 added
+// this algorithm recursively finds a parent and splits into smaller subtrees until base case
 class Solution {
     int preorderIndex;
     Map<Integer, Integer> inorderIndexMap;
